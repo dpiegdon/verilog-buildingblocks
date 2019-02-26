4636ddb9e810cf3fb2bbe5c866f9c8cda464e71d
David R. Piegdon <dgit@piegdon.de>
2019-02-26 23:42:58 +0000

    minimal ring oscillator that is not really of any use

--- ringoscillator.v
+++ ringoscillator.v
@@ -1,8 +1,8 @@
 `default_nettype none
 
-// Ring-Oscillator.
-// Lattice ICE40 specific.
-// May also work for ECP5 when `defining SB_LUT4 to LUT4.
+// Ring oscillator.
+// Lattice ice40 specific.
+// May also work for ecp5 when `defining sb_lut4 to lut4.
 module ringoscillator(output wire out);
 
 	wire chain_in, chain_out;
@@ -24,13 +24,31 @@ module ringoscillator(output wire out);
 		.I3(1'b0)
 	);
 
-	// One could also use a single LUT for inversion, without the
-	// extra delay. But then the oscillator is running at roughly
-	// 625MHz and the output signal is so weak that other logic
-	// might not properly pick it up. E.g. when connecting an output
-	// pin to the signal, the pin has 625MHz with -25dBm, which is
-	// 35.56mVpp! So having a single LUT delay line here seems a good
-	// choice!
+endmodule
+
+// Ring oscillator with minimal delay.
+// Lattice ice40 specific.
+// May also work for ecp5 when `defining sb_lut4 to lut4.
+//
+// In contrast to the above ringoscillator, this uses a single LUT
+// for inversion, without any extra delay.
+// This results in the output signal is running at roughly 625MHz,
+// but so weak that other logic might not properly pick it up.
+// E.g. when connecting it to an output pin, the signal has -25dBmu (~36mVpp).
+// So if in doubt use the above ringoscillator.
+module ringoscillator_minimal_delay(output wire out);
+
+	// Single LUT delay line. This also takes care that the compiler
+	// (yosys) is not removing this logic path.
+	SB_LUT4 #(
+		.LUT_INIT(16'd1)
+	) buffers (
+		.O(out),
+		.I0(out),
+		.I1(0),
+		.I2(0),
+		.I3(0)
+	);
 
 endmodule
 
