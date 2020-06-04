94804ecb7c9a6f1b34fdef02b98c8bff30f5c185
David R. Piegdon <dgit@piegdon.de>
2020-06-04 23:40:12 +0000

    further reduce logic needed for simple_spi

--- simple_spi_slave.v
+++ simple_spi_slave.v
@@ -63,33 +63,33 @@ module simple_spi_slave(
 
 	reg [$clog2(WIDTH+1)-1:0] bit_counter = 0;
 
-	reg [3:0] pin_ncs_stabilizer = 4'b1111;
-	reg [3:0] pin_clk_stabilizer = 4'b0000;
-	reg [3:0] pin_mosi_stabilizer = 0;
+	reg [2:0] pin_ncs_stabilizer = 3'b111;
+	reg [2:0] pin_clk_stabilizer = 3'b000;
+	reg [2:0] pin_mosi_stabilizer = 0;
 
-	wire cs_active = (pin_ncs_stabilizer[1:0] != 2'b11);
+	wire cs_active = (pin_ncs_stabilizer[1] == 1'b0);
 	assign cs_start = (pin_ncs_stabilizer[1:0] == 2'b01);
 	assign cs_stop  = (pin_ncs_stabilizer[1:0] == 2'b10);
 
 	wire sample_in = (pin_clk_stabilizer[1:0] == 2'b10);
 	wire latch_out = (pin_clk_stabilizer[1:0] == 2'b01);
 
-	assign pin_miso_en = cs_active && !pin_ncs;
+	assign pin_miso_en = !pin_ncs;
 
 	assign value_valid = (cs_stop && (bit_counter == WIDTH));
 
 
-	always @(negedge system_clk) begin
-		pin_ncs_stabilizer  <= { pin_ncs,  pin_ncs_stabilizer[3:1]  };
-		pin_clk_stabilizer  <= { CPOL^pin_clk,  pin_clk_stabilizer[3:1]  };
-		pin_mosi_stabilizer <= { pin_mosi, pin_mosi_stabilizer[3:1] };
+	always @(posedge system_clk) begin
+		pin_ncs_stabilizer  <= { pin_ncs,  pin_ncs_stabilizer[2:1]  };
+		pin_clk_stabilizer  <= { CPOL^pin_clk,  pin_clk_stabilizer[2:1]  };
+		pin_mosi_stabilizer <= { pin_mosi, pin_mosi_stabilizer[2:1] };
 
 		if(cs_active) begin
 			if(cs_start) begin
 				datum <= value_miso;
 				pin_miso <= value_miso[WIDTH-1];
 				bit_counter <= 0;
-			end else if(!cs_stop) begin
+			end else begin
 				if(bit_counter < WIDTH) begin
 					if(sample_in) begin
 						datum <= {datum[WIDTH-2:0], pin_mosi_stabilizer[0]};
